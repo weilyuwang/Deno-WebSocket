ff61074beab915cbd904ea0636f4dd516f1c80d1
weilyuwang <weilyuwang@gmail.com>
2020-07-18 23:39:47 -0400

    :zap: server receives message

--- ws/chatroom.ts
+++ ws/chatroom.ts
@@ -1,4 +1,7 @@
-import { WebSocket } from "https://deno.land/std/ws/mod.ts";
+import {
+  WebSocket,
+  isWebSocketCloseEvent,
+} from "https://deno.land/std/ws/mod.ts";
 import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 let sockets = new Map<string, WebSocket>();
@@ -10,7 +13,20 @@ const chatConnection = async (ws: WebSocket) => {
   const uid = v4.generate();
   sockets.set(uid, ws);
 
-  console.log(sockets);
+  for await (const ev of ws) {
+    console.log(ev);
+
+    // delete socket if connection closed
+    if (isWebSocketCloseEvent(ev)) {
+      sockets.delete(uid);
+    }
+
+    // create ev object if ev is string
+    if (typeof ev === "string") {
+      let evObj = JSON.parse(ev);
+      console.log(evObj);
+    }
+  }
 };
 
 export { chatConnection };
